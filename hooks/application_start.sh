9dba680a76749ddad4514a1006286eec915141b6
kazunori wakamatsu <Kazunori.Wakamatsu@jrits.ricoh.co.jp>
2018-09-04 08:49:53 +0900

    fix app start command

--- hooks/application_start.sh
+++ hooks/application_start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
  
-app_path="/usr/src/app/bin/www"
+app_path="/usr/src/app/index.js"
 forever_log="/var/log/app/forever.log"
 app_access_log="/var/log/app/access.log"
 app_error_log="/var/log/app/error.log"
@@ -9,4 +9,4 @@ forever start \
   -l "$forever_log" \
   -o "$app_access_log" \
   -e "$app_error_log" \
-  "node /usr/src/app/index.js"
+  "$app_path"
