804128a15b6ab87bc004045bc1ba954c8ea9661b
kazunori wakamatsu <Kazunori.Wakamatsu@jrits.ricoh.co.jp>
2018-09-05 08:21:57 +0900

    fix start command

--- hooks/application_start.sh
+++ hooks/application_start.sh
@@ -5,8 +5,10 @@ forever_log="/var/log/app/forever.log"
 app_access_log="/var/log/app/access.log"
 app_error_log="/var/log/app/error.log"
  
-forever start \
-  -l "$forever_log" \
-  -o "$app_access_log" \
-  -e "$app_error_log" \
-  "$app_path"
+# forever start \
+#   -l "$forever_log" \
+#   -o "$app_access_log" \
+#   -e "$app_error_log" \
+#   "$app_path"
+
+node "$app_path"
