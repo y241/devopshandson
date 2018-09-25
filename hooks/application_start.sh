95874fec8e8b548b29ee5d3817e3b542c7610a9e
hishida <hiroshi.ishida@jp.ricoh.com>
2018-09-25 19:03:56 +0900

    changed the way to start app.

--- hooks/application_start.sh
+++ hooks/application_start.sh
@@ -4,11 +4,10 @@ app_path="/usr/src/app/index.js"
 forever_log="/var/log/app/forever.log"
 app_access_log="/var/log/app/access.log"
 app_error_log="/var/log/app/error.log"
- 
-# forever start \
-#   -l "$forever_log" \
-#   -o "$app_access_log" \
-#   -e "$app_error_log" \
-#   "$app_path"
 
-forever restart /usr/src/app/index.js
+forever stopall
+forever start \
+  -l "$forever_log" \
+  -o "$app_access_log" \
+  -e "$app_error_log" \
+  "$app_path"
