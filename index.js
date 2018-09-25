e0c9148e36d9501e24afb8d5cded1152414faa54
hishida <hiroshi.ishida@jp.ricoh.com>
2018-09-25 19:08:28 +0900

    Changed text.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World Handson !!'))
+app.get('/', (req, res) => res.send('Hello DevOps Handson !!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
